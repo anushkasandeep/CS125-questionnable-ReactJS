d50ca4e558aa4ddd0f60bf3a79dd7b0bb8bf97c1
anushkasandeep <sandeep7@illinois.edu>
2019-12-15 01:45:40 -0600

    Create a new file for each react component. Do the same for ace editor

---
+++ src/Ace.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <AceEditor />
+            </div>
+        );
+    }
+}
+// AceEditor is written in the div tags to add AceEditor to the webpage
--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import React, { Component } from 'react';
 import ReactAce from 'react-ace-editor';
-import logo from './logo.svg';
 import './App.css';
-// this class for testing whether ReactAce works and looks like the code compiles
-class CodeEditor extends Component {
-  constructor() {
-    super();
-    this.onChange = this.onChange.bind(this);
-  }
-  // trying to create a new instance of ace editor
-  onChange(newValue, e) {
-    console.log(newValue, e);
-
-    const editor = this.ace.editor; //The editor object is from Ace's API
-    console.log(editor.getValue()); // outputs the value of the editor
-  }
-  // rendering the whole instance 
-  render() {
-    return (
-      <ReactAce
-        mode="javascript"
-        theme="eclipse"
-        setReadOnly="false"
-        onChange={this.onChange}
-        style={{height: '400px'}}
-        ref={instance => {this.ace = instance;}}
-      />
-    );
-  }
-}
 
 //this class is for the buttons and tables
-//TODO: took out the code for creating the textbox since it seemed buggy. will figure that out later once i'm sure react ace works
-//TODO: i should probably render the textbox and buttons in the ace editor tag in the class "CodeEditor". I think that's the whole point of ace editor.
-// as students type their code in the regular textbox, the same code is being typed in the actual code (probably as a string) at the backend which parses 
-//the code and runs in the compiler?
 class App extends Component {
   constructor(props) {
     super (props);
@@ -52,10 +20,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <button onClick={this.handleClick}>
             {this.state.isToggleOn ? 'Your Code' : 'Your Solution'}
           </button>
@@ -65,5 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
-export default CodeEditor;
+export default App
