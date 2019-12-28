c92d2565f478830589aa6a218d6a6e5797433d71
anushkasandeep <sandeep7@illinois.edu>
2019-12-28 14:05:48 -0800

    Fix typo which should remove those 12 errors in the program

--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -7,10 +7,10 @@ class ThemeSwitch extends Component {
             active: 'false'
         };
 
-        this.css = 
+        this.css = `
         html { filter: invert(100%); background: fefefe; }
         * {background-color: inherit }
-        img:not([src*="svg"]), video { filter: invert(100%) } ;
+        img:not([src*="svg"]), video { filter: invert(100%) }`;
     }
     isActive = () => {
         this.setState({
