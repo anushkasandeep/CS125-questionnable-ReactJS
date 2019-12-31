f970971da8a35714e55af65f3591c1a1d6f045dc
anushkasandeep <sandeep7@illinois.edu>
2019-12-30 23:56:13 -0800

    Made themeswitcher but need to rearrange layout and shift some code around to make the ace editor and a few more buttons visible. This program is definitely under the works and isn't ready for code review yet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AceEditor from 'react-ace';
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-kotlin";
 import "ace-builds/src-noconflict/theme-dracula";
-import ThemeSwitch from './Components/ThemeSwitch';
 
 class App extends Component {
   constructor(props) {
@@ -30,7 +29,6 @@ class App extends Component {
         <button onClick={this.handleCluck}>
           {this.state.isTog ? 'Java': 'Kotlin'}
         </button>
-        <ThemeSwitch/>
         <AceEditor 
           mode="java"
           mode="kotlin"
@@ -42,4 +40,51 @@ class App extends Component {
     );
   }
 }
+
+function ThemeSwitcher() {
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  React.useEffect(() => {
+      localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+      const isReturningUser = "dark" in localStorage;
+      const savedMode = JSON.parse(localStorage.getItem("dark"));
+      const userPrefersDark = getPrefColorScheme();
+
+      if (isReturningUser) {
+          return savedMode;
+      } else if (userPrefersDark) {
+          return true;
+      } else {
+          return false;
+      }
+
+      function getPrefColorScheme() {
+          if (!window.matchMedia) return;
+
+          return window.matchMedia("(prefers-color-scheme: dark)").matches;
+      }
+  }
+  return (
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+          <nav>
+              <div className="toggle-container">
+                  <span style={{ color : darkMode ? "grey" : "yellow"}}>☀︎</span>
+                  <span className="toggle">
+                      <input
+                          checked={darkMode}
+                          onChange={() => setDarkMode(prevMode => !prevMode)}
+                          id="checkbox"
+                          className="checkbox"
+                          type="checkbox"
+                      />
+                      <label htmlFor="checkbox" />
+                  </span>
+              </div>
+          </nav>
+      </div>
+  );
+}
+//ReactDOM.render(<App />, document.getElementById('root'));
 export default App
