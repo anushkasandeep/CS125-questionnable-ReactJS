5fe7621a6fc311f173f431495411a5c5b0be40b8
anushkasandeep <sandeep7@illinois.edu>
2020-01-03 12:08:29 -0800

    visible theme switching button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import "./App.css";
 import AceEditor from 'react-ace';
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-kotlin";
@@ -11,6 +11,7 @@ class App extends Component {
     this.state = {isToggleOn: true};
     this.handleClick = this.handleClick.bind(this);
     this.handleCluck = this.handleCluck.bind(this);
+    this.themeSwitch = this.themeSwitch.bind(this);
   }
   handleClick() {
     this.setState(state => ({
@@ -23,15 +24,32 @@ class App extends Component {
     }));
   }
 
+  themeSwitch() {
+    this.setState(state => ({
+      isToggle: !state.isToggle
+    }));
+  }
   render() {
     return (
       <div className="App">
+        <div className="toggle-container">
+          <span style={{color: this.themeSwitch ? "black" : "yellow"}}>☀︎</span>
+          <span className="toggle">
+            <input 
+              id="checkbox"
+              className="checkbox"
+              type="checkbox"
+            />
+            <label htmlFor="checkbox" />
+          </span>
+        </div>
         <button onClick={this.handleCluck}>
           {this.state.isTog ? 'Java': 'Kotlin'}
         </button>
         <AceEditor 
           mode="java"
-          mode="kotlin"
+          //mode="kotlin"
+          theme="dracula"
         />
         <button onClick={this.handleClick}>
           {this.state.isToggleOn ? 'Your Code' : 'Your Solution'}
@@ -39,52 +57,5 @@ class App extends Component {
       </div>
     );
   }
-}
-
-function ThemeSwitcher() {
-  const [darkMode, setDarkMode] = React.useState(getInitialMode());
-  React.useEffect(() => {
-      localStorage.setItem("dark", JSON.stringify(darkMode));
-  }, [darkMode]);
-
-  function getInitialMode() {
-      const isReturningUser = "dark" in localStorage;
-      const savedMode = JSON.parse(localStorage.getItem("dark"));
-      const userPrefersDark = getPrefColorScheme();
-
-      if (isReturningUser) {
-          return savedMode;
-      } else if (userPrefersDark) {
-          return true;
-      } else {
-          return false;
-      }
-
-      function getPrefColorScheme() {
-          if (!window.matchMedia) return;
-
-          return window.matchMedia("(prefers-color-scheme: dark)").matches;
-      }
-  }
-  return (
-      <div className={darkMode ? "dark-mode" : "light-mode"}>
-          <nav>
-              <div className="toggle-container">
-                  <span style={{ color : darkMode ? "grey" : "yellow"}}>☀︎</span>
-                  <span className="toggle">
-                      <input
-                          checked={darkMode}
-                          onChange={() => setDarkMode(prevMode => !prevMode)}
-                          id="checkbox"
-                          className="checkbox"
-                          type="checkbox"
-                      />
-                      <label htmlFor="checkbox" />
-                  </span>
-              </div>
-          </nav>
-      </div>
-  );
-}
-//ReactDOM.render(<App />, document.getElementById('root'));
+}      
 export default App
--- src/index.js
+++ src/index.js
@@ -4,54 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function ThemeSwitcher() {
-    const [darkMode, setDarkMode] = React.useState(getInitialMode());
-    React.useEffect(() => {
-        localStorage.setItem("dark", JSON.stringify(darkMode));
-    }, [darkMode]);
-
-    function getInitialMode() {
-        const isReturningUser = "dark" in localStorage;
-        const savedMode = JSON.parse(localStorage.getItem("dark"));
-        const userPrefersDark = getPrefColorScheme();
-
-        if (isReturningUser) {
-            return savedMode;
-        } else if (userPrefersDark) {
-            return true;
-        } else {
-            return false;
-        }
-
-        function getPrefColorScheme() {
-            if (!window.matchMedia) return;
-
-            return window.matchMedia("(prefers-color-scheme: dark)").matches;
-        }
-    }
-    return (
-        <div className={darkMode ? "dark-mode" : "light-mode"}>
-            <nav>
-                <div className="toggle-container">
-                    <span style={{ color : darkMode ? "grey" : "yellow"}}>☀︎</span>
-                    <span className="toggle">
-                        <input
-                            checked={darkMode}
-                            onChange={() => setDarkMode(prevMode => !prevMode)}
-                            id="checkbox"
-                            className="checkbox"
-                            type="checkbox"
-                        />
-                        <label htmlFor="checkbox" />
-                    </span>
-                </div>
-            </nav>
-        </div>
-    );
-}
-
-
-ReactDOM.render(<ThemeSwitcher />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
