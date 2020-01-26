444b1ad84836acc81a93619393680b6f96ce1f23
anushkasandeep <sandeep7@illinois.edu>
2020-01-26 01:25:32 -0600

    Replace \r with \n, string manipulation, make it kotlin oriented

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "ace-builds/src-noconflict/mode-kotlin";
 import "ace-builds/src-noconflict/theme-dracula";
 import "./components/outputBox";
 //import Footer from './components/outputBox';
-//import { Segment } from "semantic-ui-react";
+//import { Segment } from "semantic-ui-react";3
 
 class App extends Component {
   constructor(props) {
@@ -37,9 +37,17 @@ class App extends Component {
 
   codeToString() {
     var studentCode = JSON.stringify(this.theCode.current.editor.getValue());
-    this.componentDidMount(studentCode);
+    var newStudentCode = studentCode.replace("\r", "\n");
+    this.componentDidMount(newStudentCode);
+    console.log(newStudentCode);
   }
 
+  //"fun main() { \r\nprintln(\"Hello World\" \r\n}"
+  //"fun main() { \r\nprintln(\"Hello World\" \r\n}"
+  //
+  /*
+  -----------------------------------------------------------------------------
+  FOR JAVA
   async componentDidMount(args) {
     const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
       method: 'POST',
@@ -51,6 +59,55 @@ class App extends Component {
       errorMessage:finResponse,
     })
   }
+  -----------------------------------------------------------------------------
+  */
+
+  /*
+  async componentDidMount(args) { 
+    const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
+      method: 'POST',
+      headers: { 'Content-Type' : 'application/json' },
+      body: JSON.stringify({ "label": "questionable-kotlin",
+                            "arguments":{"execution":{"klass":"MainKt","method":"main()"}},
+                            "sources":[{"path":"Main.kt","contents":args}],
+                            "tasks":["execute","kompile"]}),
+    })
+    //alert(JSON.stringify(await response.json()));
+    var finResponse = JSON.stringify(await response.json());
+    this.setState({
+      errorMessage:args
+    })
+  }*/
+
+  async componentDidMount(args) { 
+    const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
+      method: 'POST',
+      headers: { 'Content-Type' : 'application/json; charset=utf-8' },
+      body: JSON.stringify({ "label": "questionable-kotlin",
+                            "arguments":{"execution":{"klass":"MainKt","method":"main()"}},
+                            "sources":[{"path":"Main.kt","contents":args}],
+                            "tasks":["execute","kompile"]}),
+    })
+    //alert(JSON.stringify(await response.json()));
+    var finResponse = JSON.stringify(await response.json());
+    this.setState({
+      errorMessage:finResponse
+    })
+  }
+
+
+  /*
+  without
+  curl --request POST \
+  --url https://cs125-cloud.cs.illinois.edu/jeed/ \
+  --header 'content-type: application/json; charset=utf-8' \
+  --data '{"label":"questionable-kotlin",
+          "arguments":{"execution":{"klass":"MainKt","method":"main()"}},
+ 					"sources":[{"path":"Main.kt",
+					"contents":"fun main() {\n  val sum = add(1, 2)\n  println(\"Hello, Kotlin! $sum\")\n}\n\nfun add(a: Int, b: Int): Int {\n  return a + b\n}\n"}],
+ 					"tasks":["execute","kompile"]}'
+
+  */
 
 
   render() {    
@@ -73,8 +130,8 @@ class App extends Component {
           {this.state.isTog ? 'Java': 'Kotlin'}
         </button>        
         <AceEditor
-        mode="java" 
-        //mode="kotlin"
+        //mode="java" 
+        mode="kotlin"
         theme="dracula"
         ref={this.theCode}
         />
@@ -93,24 +150,3 @@ class App extends Component {
 }  
     
 export default App
-
-
-/*
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-i write code in ace editor. editor's state keeps changing. i don't want instantaneous change of state.
-when i click the run button, the final instance of ace editor should be passed into the function probably as an argument and the function would get the value of that instance
-that function return the getValue as a string
-
-2 functions! 
-function 1: function 1 stores the current state of ace editor. 
-
-ace editor passes its current instance to funct1. funct1 holds state of ace editor and returns the value of that instance
-button activated functTwo which 
-
-you actually didn't need the first function to constantly store the state.
-refs would do that job for you 
-refs are appropriate to use when a function wants to access something from the rendered elements.
-this is the opposite of what usually happens where the rendered elements access the functions
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-01/21
-*/
