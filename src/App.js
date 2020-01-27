5a6f41181dc22b071dce0ea091fb6f8fcb498833
anushkasandeep <sandeep7@illinois.edu>
2020-01-27 01:15:52 -0600

    More planning, less coding

--- src/App.js
+++ src/App.js
@@ -37,17 +37,14 @@ class App extends Component {
 
   codeToString() {
     var studentCode = JSON.stringify(this.theCode.current.editor.getValue());
-    var newStudentCode = studentCode.replace("\r", "\n");
+    var newStudentCode = studentCode.replace("\r", "\n")
+    
     this.componentDidMount(newStudentCode);
-    console.log(newStudentCode);
+    
   }
-
-  //"fun main() { \r\nprintln(\"Hello World\" \r\n}"
-  //"fun main() { \r\nprintln(\"Hello World\" \r\n}"
-  //
-  /*
-  -----------------------------------------------------------------------------
-  FOR JAVA
+  
+ 
+  /*FOR JAVA
   async componentDidMount(args) {
     const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
       method: 'POST',
@@ -59,26 +56,8 @@ class App extends Component {
       errorMessage:finResponse,
     })
   }
-  -----------------------------------------------------------------------------
   */
-
-  /*
-  async componentDidMount(args) { 
-    const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
-      method: 'POST',
-      headers: { 'Content-Type' : 'application/json' },
-      body: JSON.stringify({ "label": "questionable-kotlin",
-                            "arguments":{"execution":{"klass":"MainKt","method":"main()"}},
-                            "sources":[{"path":"Main.kt","contents":args}],
-                            "tasks":["execute","kompile"]}),
-    })
-    //alert(JSON.stringify(await response.json()));
-    var finResponse = JSON.stringify(await response.json());
-    this.setState({
-      errorMessage:args
-    })
-  }*/
-
+  
   async componentDidMount(args) { 
     const response = await fetch("https://cs125-cloud.cs.illinois.edu/jeed/", {
       method: 'POST',
@@ -90,25 +69,11 @@ class App extends Component {
     })
     //alert(JSON.stringify(await response.json()));
     var finResponse = await response.json();
-    var fResponse = JSON.stringify(finResponse)
+    var fResponse = JSON.stringify(finResponse);
     this.setState({
       errorMessage:fResponse
     })
   }
-
-
-  /*
-  without
-  curl --request POST \
-  --url https://cs125-cloud.cs.illinois.edu/jeed/ \
-  --header 'content-type: application/json; charset=utf-8' \
-  --data '{"label":"questionable-kotlin",
-          "arguments":{"execution":{"klass":"MainKt","method":"main()"}},
- 					"sources":[{"path":"Main.kt",
-					"contents":"fun main() {\n  val sum = add(1, 2)\n  println(\"Hello, Kotlin! $sum\")\n}\n\nfun add(a: Int, b: Int): Int {\n  return a + b\n}\n"}],
- 					"tasks":["execute","kompile"]}'
-
-  */
 
 
   render() {    
